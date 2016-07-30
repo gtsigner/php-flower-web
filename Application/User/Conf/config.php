e10ee3076e4b2e7e8afbebfeda4167fb78342479
1716771371@qq.com <1716771371@qq.com>
2016-07-30 11:16:28 +0800

    细节

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'zYmb?_@K&Bo.kSjhuW>"HOU{<Q7rc/3+}g2#MP:R'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/client_08'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:zhaojun@10.1.56.115:3306/client_08'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'dev_'); // 数据表前缀，使用Model方式调用API必须配置此项
