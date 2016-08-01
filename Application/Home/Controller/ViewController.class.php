d7485bd4e1f568e387276d09ebe72c37aa296cfb
leichuqiao <yanuwoaini@126.com>
2016-08-01 16:41:17 +0800

    2016-8-1-16-41

--- Application/Home/Controller/ViewController.class.php
+++ Application/Home/Controller/ViewController.class.php
@@ -1,11 +1,13 @@
 <?php
-
-namespace Home\Controller;
-
-class ViewController extends HomeController
-{
-    public function travel_play()
-    {
-        $this->display();
+    namespace Home\Controller;
+    class ViewController extends HomeController{
+        public function travel_play(){
+            $this->display();
+        }
+        public function jq_news(){
+            $this->display();
+        }
+        public function travel_method(){
+            $this->display();
+        }
     }
-}
